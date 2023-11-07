07171ddc71c5a6dd208876a03ee3b76fe4d7732c
Balaji <balaji@nextleinfotech.com>
2023-11-07 10:32:17 +0530

    Solution added for Task 4. Profile Update.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
